35f16aaa46166701ae42acfa35161bd4a95c0921
Zheka1234 <102152291+Zheka1234@users.noreply.github.com>
2023-03-26 13:49:13 +0300

    fix

--- database/src/main/java/com/miskevich/repository/UserRepository.java
+++ database/src/main/java/com/miskevich/repository/UserRepository.java
@@ -1,14 +1,13 @@
 package com.miskevich.repository;
 
 import com.miskevich.entity.User;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    List<User> findAll(Sort sort);
+
 
 }
